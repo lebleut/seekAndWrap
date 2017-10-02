b0c17d2322c09e3655c2e3a1001ba28a6ea178e0
Jamel Eddine Zarga <jamel.zarga@gmail.com>
2017-10-02 21:29:08 +0100

    Add the plugin code ( seek-and-wrap.js ) and test folder

---
+++ src/seek-and-wrap.js
@@ -0,0 +1,69 @@
+
+$.fn.seekAndWrap = function( opt ){
+  var defaut = {
+    "search": "",
+    "attribs":null,
+    "tag":"div",
+    "class":"keyword"
+  };
+
+  var params = $.extend(defaut, opt);
+  
+  function wrapIt( elem, params ){
+    elem.contents().each(function(){
+      if( this.nodeType == 3 ){
+        var content = $(this).text();
+
+        var before_search = '^|\\W'; // ^|\W : start OR non-word character
+        var after_search = '$|\\W';  // $|\\W : end OR non-word character
+
+        var pattern = new RegExp('('+before_search+')('+params.search+')('+after_search+')','ig');
+
+        var new_elem;
+
+        var replacer = function(match, p1, p2, p3, offset, string){
+          // p1 group before the word
+          // p2 the word needed
+          // p3 group after the word
+
+          new_elem = $("<"+params.tag+" />");
+
+          new_elem.text( p2 );
+
+          if( params.attribs != null && Array.isArray(params.attribs) ){
+            params.attribs.forEach(function(attr, index){
+              return new_elem.attr(attr.name, attr.value);
+            });
+
+          }
+
+          if(params.class != ""){
+            new_elem.addClass(params.class);
+          }
+
+          return p1+new_elem.prop('outerHTML')+p3;
+        }
+
+        content = content.replace(pattern, replacer);
+
+        $(this).replaceWith(content);
+
+      }else{
+        wrapIt( $(this), params );
+      }
+
+    });
+  }
+  
+  return this.each(function(index){
+    if( "" === params.search ){
+      console.log("No param yet");
+      return false;
+    }
+
+    wrapIt($(this), params );
+
+  });
+
+
+}
---
+++ test/script.js
@@ -0,0 +1,38 @@
+
+$("body").seekAndWrap({
+  "tag":"span",
+  "search":"web",
+  "attribs":[
+    {
+      "name" : "data-tooltip",
+      "value" : "WWW : World Wide Web"
+    },
+    {
+      "name" : "title",
+      "value" : "test"
+    }
+  ]
+
+});
+
+$("body").seekAndWrap({
+  "tag":"div",
+  "class":"ttp",
+  "search":"js",
+  "attribs":[
+    {
+      "name" : "data-tooltip",
+      "value" : "JavaScript"
+    }
+  ]
+
+});
+
+// Now use the new state to create jquery tooltips
+$( document ).tooltip({
+  "items":".keyword, .ttp",
+  "content": function(){
+    return $(this).attr("data-tooltip");
+  },
+  "track": true
+});
